34acce120a22ced4631019245f351db8ea10bbde
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2017-11-21 10:29:20 +0200

    changed test script to make use of exit codes

--- test_build.sh
+++ test_build.sh
@@ -1,7 +1,17 @@
 #/bin/sh
 
+function test {
+  echo "+ $@"
+  "$@"
+  local status=$?
+  if [ $status -ne 0 ]; then
+    exit $status
+  fi
+  return $status
+}
+
 GIT_VERSION=`cd ${GOPATH}/src/github.com/elgatito/elementum; git describe --tags`
 cd $GOPATH
-set -x
-go build -ldflags="-w -X github.com/elgatito/elementum/util.Version=\"${GIT_VERSION}\"" github.com/elgatito/elementum
-cp -rf elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/; cp -rf elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
+# set -x
+test go build -ldflags="-w -X github.com/elgatito/elementum/util.Version=\"${GIT_VERSION}\"" github.com/elgatito/elementum
+test cp -rf elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/; cp -rf elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
