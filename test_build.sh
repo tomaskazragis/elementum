92fffca7edae0fbd1c0f3827026cc4d7da239864
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2017-11-23 11:45:26 +0200

    fix test build script for proper exit codes and copy-paste paths

--- test_build.sh
+++ test_build.sh
@@ -14,4 +14,6 @@ GIT_VERSION=`cd ${GOPATH}/src/github.com/elgatito/elementum; git describe --tags
 cd $GOPATH
 # set -x
 test go build -ldflags="-w -X github.com/elgatito/elementum/util.Version=\"${GIT_VERSION}\"" -o /var/tmp/elementum github.com/elgatito/elementum
-test cp -rf /var/tmp/elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/; cp -rf /var/tmp/elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
+test chmod +x /var/tmp/elementum
+test cp -rf /var/tmp/elementum $HOME/.kodi/addons/plugin.video.elementum/resources/bin/linux_x64/
+test cp -rf /var/tmp/elementum $HOME/.kodi/userdata/addon_data/plugin.video.elementum/bin/linux_x64/
