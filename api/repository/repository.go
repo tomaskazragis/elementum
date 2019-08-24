1b8226677af33c96fd8c83df82e42aec1fa76092
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2019-08-24 10:16:25 +0200

    removed dependency on github package

--- api/repository/repository.go
+++ api/repository/repository.go
@@ -2,30 +2,128 @@ package repository
 
 import (
 	"bufio"
-	"context"
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/anacrolix/missinggo/perf"
 	"github.com/elgatito/elementum/config"
 	"github.com/elgatito/elementum/proxy"
 	"github.com/elgatito/elementum/xbmc"
 	"github.com/gin-gonic/gin"
-	"github.com/google/go-github/github"
 	"github.com/op/go-logging"
 )
 
+// Timestamp represents a time that can be unmarshalled from a JSON string
+// formatted as either an RFC3339 or Unix timestamp. This is necessary for some
+// fields since the GitHub API is inconsistent in how it represents times. All
+// exported methods of time.Time can be called on Timestamp.
+type Timestamp struct {
+	time.Time
+}
+
+// ReleaseAsset represents a GitHub release asset in a repository.
+type ReleaseAsset struct {
+	ID                 int64     `json:"id,omitempty"`
+	URL                string    `json:"url,omitempty"`
+	Name               string    `json:"name,omitempty"`
+	Label              string    `json:"label,omitempty"`
+	State              string    `json:"state,omitempty"`
+	ContentType        string    `json:"content_type,omitempty"`
+	Size               int       `json:"size,omitempty"`
+	DownloadCount      int       `json:"download_count,omitempty"`
+	CreatedAt          Timestamp `json:"created_at,omitempty"`
+	UpdatedAt          Timestamp `json:"updated_at,omitempty"`
+	BrowserDownloadURL string    `json:"browser_download_url,omitempty"`
+	Uploader           User      `json:"uploader,omitempty"`
+	NodeID             string    `json:"node_id,omitempty"`
+}
+
+// User represents a GitHub user.
+type User struct {
+	Login                   string    `json:"login,omitempty"`
+	ID                      int64     `json:"id,omitempty"`
+	NodeID                  string    `json:"node_id,omitempty"`
+	AvatarURL               string    `json:"avatar_url,omitempty"`
+	HTMLURL                 string    `json:"html_url,omitempty"`
+	GravatarID              string    `json:"gravatar_id,omitempty"`
+	Name                    string    `json:"name,omitempty"`
+	Company                 string    `json:"company,omitempty"`
+	Blog                    string    `json:"blog,omitempty"`
+	Location                string    `json:"location,omitempty"`
+	Email                   string    `json:"email,omitempty"`
+	Hireable                bool      `json:"hireable,omitempty"`
+	Bio                     string    `json:"bio,omitempty"`
+	PublicRepos             int       `json:"public_repos,omitempty"`
+	PublicGists             int       `json:"public_gists,omitempty"`
+	Followers               int       `json:"followers,omitempty"`
+	Following               int       `json:"following,omitempty"`
+	CreatedAt               Timestamp `json:"created_at,omitempty"`
+	UpdatedAt               Timestamp `json:"updated_at,omitempty"`
+	SuspendedAt             Timestamp `json:"suspended_at,omitempty"`
+	Type                    string    `json:"type,omitempty"`
+	SiteAdmin               bool      `json:"site_admin,omitempty"`
+	TotalPrivateRepos       int       `json:"total_private_repos,omitempty"`
+	OwnedPrivateRepos       int       `json:"owned_private_repos,omitempty"`
+	PrivateGists            int       `json:"private_gists,omitempty"`
+	DiskUsage               int       `json:"disk_usage,omitempty"`
+	Collaborators           int       `json:"collaborators,omitempty"`
+	TwoFactorAuthentication bool      `json:"two_factor_authentication,omitempty"`
+	LdapDn                  string    `json:"ldap_dn,omitempty"`
+
+	// API URLs
+	URL               string `json:"url,omitempty"`
+	EventsURL         string `json:"events_url,omitempty"`
+	FollowingURL      string `json:"following_url,omitempty"`
+	FollowersURL      string `json:"followers_url,omitempty"`
+	GistsURL          string `json:"gists_url,omitempty"`
+	OrganizationsURL  string `json:"organizations_url,omitempty"`
+	ReceivedEventsURL string `json:"received_events_url,omitempty"`
+	ReposURL          string `json:"repos_url,omitempty"`
+	StarredURL        string `json:"starred_url,omitempty"`
+	SubscriptionsURL  string `json:"subscriptions_url,omitempty"`
+}
+
+// Release represents a GitHub release in a repository.
+type Release struct {
+	TagName         string `json:"tag_name,omitempty"`
+	TargetCommitish string `json:"target_commitish,omitempty"`
+	Name            string `json:"name,omitempty"`
+	Body            string `json:"body,omitempty"`
+	Draft           bool   `json:"draft,omitempty"`
+	Prerelease      bool   `json:"prerelease,omitempty"`
+
+	// The following fields are not used in CreateRelease or EditRelease:
+	ID          int64          `json:"id,omitempty"`
+	CreatedAt   Timestamp      `json:"created_at,omitempty"`
+	PublishedAt Timestamp      `json:"published_at,omitempty"`
+	URL         string         `json:"url,omitempty"`
+	HTMLURL     string         `json:"html_url,omitempty"`
+	AssetsURL   string         `json:"assets_url,omitempty"`
+	Assets      []ReleaseAsset `json:"assets,omitempty"`
+	UploadURL   string         `json:"upload_url,omitempty"`
+	ZipballURL  string         `json:"zipball_url,omitempty"`
+	TarballURL  string         `json:"tarball_url,omitempty"`
+	Author      User           `json:"author,omitempty"`
+	NodeID      string         `json:"node_id,omitempty"`
+}
+
 const (
-	githubUserContentURL = "http://elementum.surge.sh/packages/%s/%s"
-	releaseChangelog     = "[B]%s[/B] - %s\n%s\n\n"
+	githubUserContentURL   = "http://elementum.surge.sh/packages/%s/%s"
+	githubLatestReleaseURL = "https://api.github.com/repos/%s/%s/releases/latest"
+	githubReleasesURL      = "https://api.github.com/repos/%s/%s/releases"
+
+	releaseChangelog = "[B]%s[/B] - %s\n%s\n\n"
 )
 
 var (
@@ -33,10 +131,6 @@ var (
 	addonChangelogRE = regexp.MustCompile(`changelog.*\.txt`)
 	log              = logging.MustGetLogger("repository")
 )
-
-func getGithubClient() *github.Client {
-	return github.NewClient(nil)
-}
 
 func getLastRelease(user string, repository string) string {
 	defer perf.ScopeTimer()()
@@ -53,21 +147,22 @@ func getLastRelease(user string, repository string) string {
 	return string(bodyBytes)
 }
 
-func getReleaseByTag(user string, repository string, tagName string) *github.RepositoryRelease {
+func getLatestRelease(user string, repository string) *Release {
 	defer perf.ScopeTimer()()
 
-	client := getGithubClient()
-	releases, _, err := client.Repositories.ListReleases(context.TODO(), user, repository, nil)
-	if err != nil {
-		return nil
-	}
+	res, _ := http.Get(fmt.Sprintf(githubLatestReleaseURL, user, repository))
+	var release Release
+	json.NewDecoder(res.Body).Decode(&release)
+	return &release
+}
 
-	for _, release := range releases {
-		if *release.TagName == tagName {
-			return release
-		}
-	}
-	return nil
+func getReleases(user string, repository string) []Release {
+	defer perf.ScopeTimer()()
+
+	res, _ := http.Get(fmt.Sprintf(githubLatestReleaseURL, user, repository))
+	var releases []Release
+	json.NewDecoder(res.Body).Decode(&releases)
+	return releases
 }
 
 func getAddonXML(user string, repository string) (string, error) {
@@ -179,31 +274,24 @@ func GetAddonFilesHead(ctx *gin.Context) {
 
 func addonZip(ctx *gin.Context, user string, repository string, lastReleaseTag string) {
 	defer perf.ScopeTimer()()
-	release := getReleaseByTag(user, repository, lastReleaseTag)
+	release := getLatestRelease(user, repository)
 	// if there a release with an asset that matches a addon zip, use it
 	if release == nil {
-		return
-	}
-
-	client := getGithubClient()
-	assets, _, err := client.Repositories.ListReleaseAssets(context.TODO(), user, repository, *release.ID, nil)
-	if err != nil {
-		ctx.Err()
 		return
 	}
 
 	platformStruct := xbmc.GetPlatform()
 	platform := platformStruct.OS + "_" + platformStruct.Arch
 	var assetAllPlatforms string
-	for _, asset := range assets {
-		if strings.HasSuffix(*asset.Name, platform+".zip") {
-			assetPlatform := *asset.BrowserDownloadURL
+	for _, asset := range release.Assets {
+		if strings.HasSuffix(asset.Name, platform+".zip") {
+			assetPlatform := asset.BrowserDownloadURL
 			log.Infof("Using release asset for %s: %s", platform, assetPlatform)
 			ctx.Redirect(302, assetPlatform)
 			return
 		}
-		if addonZipRE.MatchString(*asset.Name) {
-			assetAllPlatforms = *asset.BrowserDownloadURL
+		if addonZipRE.MatchString(asset.Name) {
+			assetAllPlatforms = asset.BrowserDownloadURL
 			log.Infof("Found all platforms release asset: %s", assetAllPlatforms)
 			continue
 		}
@@ -217,16 +305,14 @@ func addonZip(ctx *gin.Context, user string, repository string, lastReleaseTag s
 
 func fetchChangelog(user string, repository string) (changelog string) {
 	log.Infof("Fetching add-on changelog for %s...", repository)
-	client := getGithubClient()
-	releases, _, err := client.Repositories.ListReleases(context.TODO(), user, repository, nil)
-
-	if err != nil || len(releases) == 0 {
+	releases := getReleases(user, repository)
+	if len(releases) == 0 {
 		return
 	}
 
 	changelog = repository + " changelog\n======\n\n"
 	for _, release := range releases {
-		changelog += fmt.Sprintf(releaseChangelog, release.GetTagName(), release.GetPublishedAt().Format("Jan 2 2006"), release.GetBody())
+		changelog += fmt.Sprintf(releaseChangelog, release.TagName, release.PublishedAt.Format("Jan 2 2006"), release.Body)
 	}
 
 	return
