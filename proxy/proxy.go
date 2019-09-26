e1bdcceaaad4288ced90f286e9d4bcbb784cd05a
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2019-09-26 18:27:22 +0200

    removed cloudflare functionality to separate package.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -9,15 +9,12 @@ import (
 	"net/url"
 	"regexp"
 	"strconv"
-	"strings"
-	"time"
 
 	"github.com/elgatito/elementum/config"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/ElementumOrg/cfbypass"
 	"github.com/elazarl/goproxy"
 	logging "github.com/op/go-logging"
-	"github.com/robertkrimen/otto"
 )
 
 var (
@@ -88,25 +85,10 @@ func handleResponse(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		return resp
 	}
 
-	if resp.StatusCode == 503 && (resp.Header.Get("Server") == "cloudflare-nginx" || resp.Header.Get("Server") == "cloudflare") {
-		if cfResponse, passed := solveCloudFlare(resp, ctx); passed {
-			bodyBytes := ctx.UserData.([]byte)
-			ctx.Req.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-			if cfResponse != nil {
-				for _, cookie := range cfResponse.Cookies() {
-					ctx.Req.AddCookie(cookie)
-				}
-			}
-
-			if respRetry, err := ctx.RoundTrip(ctx.Req); err == nil && respRetry != nil {
-				if set := cfResponse.Header.Get("Set-Cookie"); set != "" {
-					respRetry.Header.Add("Set-Cookie", set)
-				}
-
-				return respRetry
-			}
-		}
+	if cfResp, err := cfbypass.RunProxy(resp, ctx); err != nil {
+		log.Warningf("Could not solve the CloudFlare challenge: ", err)
+	} else if cfResp != nil {
+		return cfResp
 	}
 
 	return resp
@@ -149,176 +131,6 @@ func dumpResponse(resp *http.Response, ctx *goproxy.ProxyCtx, details bool, body
 	log.Debugf("RESPONSE:\n%s", dump)
 }
 
-func solveCloudFlare(resp *http.Response, ctx *goproxy.ProxyCtx) (*http.Response, bool) {
-	req := cloneRequest(ctx.Req)
-	for _, cookie := range resp.Cookies() {
-		req.AddCookie(cookie)
-	}
-	rurl := req.URL
-	originalURL, _ := resp.Location()
-	if originalURL == nil {
-		originalURL = req.URL
-	}
-
-	if strings.Contains(rurl.String(), "chk_jschl") {
-		// We are in deadloop
-		return nil, false
-	}
-
-	time.Sleep(time.Duration(4) * time.Second)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, false
-	}
-	buff := bytes.NewBuffer(body)
-	dom, err := goquery.NewDocumentFromReader(buff)
-	if err != nil {
-		return nil, false
-	}
-
-	host := strings.Replace(rurl.Host, ":443", "", -1)
-
-	js := dom.Find("script:contains(\"s,t,o,p,b,r,e,a,k,i,n,g\")").Text()
-	if strings.Contains(js, "parseInt") {
-		re1 := regexp.MustCompile("setTimeout\\(function\\(\\){\\s+(var s,t,o,p,b,r,e,a,k,i,n,g,f.+?\\r?\\n[\\s\\S]+?a\\.value =.+?)\\r?\\n")
-		re2 := regexp.MustCompile("a\\.value = (parseInt\\(.+?\\)).+")
-		re3 := regexp.MustCompile("\\s{3,}[a-z](?: = |\\.).+")
-		re4 := regexp.MustCompile("[\\n\\\\']")
-
-		js = re1.FindAllStringSubmatch(js, -1)[0][1]
-		js = re2.ReplaceAllString(js, re2.FindAllStringSubmatch(js, -1)[0][1])
-		js = re3.ReplaceAllString(js, "")
-		js = re4.ReplaceAllString(js, "")
-		js = strings.Replace(js, "return", "", -1)
-
-		jsEngine := otto.New()
-		data, err := jsEngine.Eval(js)
-		if err != nil {
-			return nil, false
-		}
-		checksum, err := data.ToInteger()
-		if err != nil {
-			return nil, false
-		}
-		checksum += int64(len(host))
-		if err != nil {
-			return nil, false
-		}
-
-		jschlVc, _ := dom.Find("input[name=\"jschl_vc\"]").Attr("value")
-		pass, _ := dom.Find("input[name=\"pass\"]").Attr("value")
-		jschlAnswer := strconv.Itoa(int(checksum))
-
-		u := rurl.Scheme + "://" + rurl.Host + "/cdn-cgi/l/chk_jschl"
-		ur, err := url.Parse(u)
-		q := ur.Query()
-		q.Add("jschl_vc", jschlVc)
-		q.Add("pass", pass)
-		q.Add("jschl_answer", jschlAnswer)
-		ur.RawQuery = q.Encode()
-
-		req.Header.Del("Referer")
-		req.Header.Add("Referer", rurl.String())
-
-		req.URL = ur
-		if cfResponse, err := ctx.RoundTrip(req); cfResponse != nil && err == nil {
-			return cfResponse, true
-		}
-	}
-
-	re1 := regexp.MustCompile("setTimeout\\(function\\(\\){\\s+(var s,t,o,p,b,r,e,a,k,i,n,g,f.+?\\r?\\n[\\s\\S]+?a\\.value =.+?)\\r?\\n")
-	re2 := regexp.MustCompile("\\s{3,}[a-z](?: = |\\.).+")
-	re3 := regexp.MustCompile("[\\n\\\\']")
-	re4 := regexp.MustCompile(";\\s*\\d+\\s*$")
-	re5 := regexp.MustCompile("a\\.value\\s*\\=")
-
-	res := re1.FindAllStringSubmatch(js, -1)
-	if len(res) == 0 || len(res[0]) == 0 {
-		return nil, false
-	}
-
-	js = res[0][1]
-	js = strings.Replace(js, "s,t,o,p,b,r,e,a,k,i,n,g,f,", "s,t = \""+host+"\",o,p,b,r,e,a,k,i,n,g,f,", 1)
-	js = re2.ReplaceAllString(js, "")
-	js = re3.ReplaceAllString(js, "")
-	js = re4.ReplaceAllString(js, "")
-	js = re5.ReplaceAllString(js, "return ")
-
-	jsEngine := otto.New()
-	data, err := jsEngine.Eval("(function () {" + js + "})()")
-	if err != nil {
-		return nil, false
-	}
-
-	checksum, err := data.ToInteger()
-	if err != nil {
-		return nil, false
-	}
-
-	checksum += int64(len(host))
-	if err != nil {
-		return nil, false
-	}
-
-	jschlVc, _ := dom.Find("input[name=\"jschl_vc\"]").Attr("value")
-	pass, _ := dom.Find("input[name=\"pass\"]").Attr("value")
-
-	u := rurl.Scheme + "://" + rurl.Host + "/cdn-cgi/l/chk_jschl"
-	ur, err := url.Parse(u)
-	q := ur.Query()
-	q.Add("jschl_vc", jschlVc)
-	q.Add("pass", pass)
-	ur.RawQuery = q.Encode() + "&jschl_answer=" + data.String()
-
-	req.Header.Del("Content-Type")
-	req.Header.Del("Content-Length")
-	req.Header.Del("Origin")
-
-	if originalURL != nil {
-		req.Header.Del("Referer")
-		req.Header.Add("Referer", strings.Replace(originalURL.String(), ":443", "", -1))
-	}
-
-	req.URL = ur
-	req.Method = "GET"
-	req.ContentLength = 0
-
-	if config.Get().InternalProxyLogging {
-		dumpRequest(req, ctx, true, true)
-	} else {
-		dumpRequest(req, ctx, false, true)
-	}
-	cfResponse, err := ctx.RoundTrip(req)
-	if config.Get().InternalProxyLogging {
-		dumpResponse(cfResponse, ctx, true, true)
-	} else {
-		dumpResponse(cfResponse, ctx, false, true)
-	}
-
-	if cfResponse != nil && err == nil {
-		if cfResponse.StatusCode == 503 {
-			return nil, false
-		}
-
-		return cfResponse, true
-	}
-
-	log.Debugf("Could not finish CloudFlare: %#v", err)
-	return nil, false
-}
-
-func cloneRequest(r *http.Request) *http.Request {
-	// shallow copy of the struct
-	r2 := new(http.Request)
-	*r2 = *r
-	// deep copy of the Header
-	r2.Header = make(http.Header)
-	for k, s := range r.Header {
-		r2.Header[k] = s
-	}
-	return r2
-}
-
 // StartProxy starts HTTP/HTTPS proxy for debugging
 func StartProxy() *http.Server {
 	Proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile(hostMatch))).
@@ -341,6 +153,8 @@ func StartProxy() *http.Server {
 	if config.Get().InternalDNSEnabled {
 		Proxy.Tr.Dial = CustomDial
 	}
+
+	cfbypass.LogEnabled = config.Get().InternalProxyLogging
 
 	srv := &http.Server{
 		Addr:    ":" + strconv.Itoa(ProxyPort),
